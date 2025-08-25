08d4700cf17a6a62b04d3e0cd5c76621eb62fa42
Jalgas <65507582+JalgasAralbaev@users.noreply.github.com>
2025-08-25 10:35:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ CAFE_ID = '81809'
 TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI1IiwianRpIjoiNTliNzBiNTdhYTJkMTJmNjk5OWVkMDdlYWNkMTkzMzM3NDJkYzAwMTBlOGNlNjY3NTJhODg3Mzg2YzM3MDJiZTkzZWE1MjMzNGRjZWMyN2YiLCJpYXQiOjE3NTYwNjM2NDYuOTEwOTQxLCJuYmYiOjE3NTYwNjM2NDYuOTEwOTQ0LCJleHAiOjE3ODc1OTk2NDYuOTA4MDY1LCJzdWIiOiIzODQxMTk0MjgzODE4MDkiLCJzY29wZXMiOltdfQ.Omv6u-S6viNp7JM4a_2HTtRb3ZRWxHavpVznxqwLSznK1BUdt1BPx7H-HOF4QcrcdPkpQq-MXCmzlyFdzvBuM5npnFUtsK1_a5lS61zkElh33GTJ3XYK4WniNdH24WNs96kyLBH-slccV51-Ef6bnBb2u0JYc1Kms4poQeFWE5FeU3jj8VLpnjDjA6kw0tD-jekq1V_s0WQfZQr7UcHKB0OnoYtLTByxZ2FsX-p4eip5yITTR_MJLkesxD6LUb48Ut5dtqZCMACkNOj30Dw7Ya6zOdq11Yk41d1NJ5nYbHWit2APVfQsOjVNQh4Xwh0Rz2y8TR081pyeH8nVBdDgGLnVb33koLOh9TaATlQPpgOtO1drntX5_JaAvmr2DFj75zKL7ap-EkvIs550RrnCKSJTMAgYhwe0ysVgcaYDjYrSB07wB-q5unqLpZQei6l2hAzy47rPU1FK8aVnAp7qmvUsCJKJO_JZeAZkTTRIMIbotsKdLSiItcnhqVcNlj6tlmx4Y5Tzr1vImZKy2VF7bhgBcT_HaLlsSZYlcrZdspXjWHB87Mgen0I6THYs2fwiTf9ms2tbimbncgN3n_k9BzV9e7xde7VJ3vHFtMoGm0XykGOYDQT38jXgaHqlK5UdiP05C0WdnvORap7egeYpwTRomVDWK17QhLdiSchXLXY'
 BASE_URL = f'https://api.icafecloud.com/api/v2/cafe/{CAFE_ID}'
 
+
 def calc_coins(stats):
     for_win = 3 if stats.get('win') else -2
     coins = (for_win +
@@ -14,6 +15,26 @@ def calc_coins(stats):
              stats.get('kills', 0) * 0.5 -
              stats.get('deaths', 0) * 0.3)
     return max(0, round(coins, 1))
+
+
+def send_event_data(username, win, kills, deaths, assists):
+    event_id = ''
+    url = f'{BASE_URL}/events/{event_id}/addCustomRecord'
+
+
+    headers = {
+        "Authorization": f"Bearer {TOKEN}"
+    }
+
+    data = {
+        'action': 'custom-match-stats',
+        'custom_member_account': username,
+        'custom_wins': win,
+        'custom_kills': kills,
+        'custom_assists': assists,
+        'custom_deaths': deaths
+    }
+    
 
 @app.route('/getuser', methods=['POST'])
 def get_user():
@@ -48,11 +69,13 @@ def topup():
     data = request.get_json()
     stats = data.get('stats', {})
     user_id = data.get('user_id')
+    username = data.get('username')
+    win = 1 if stats.get('win') else 0
     coins = calc_coins(stats)
     kills = stats.get('kills')
     deaths = stats.get('deaths')
     assists = stats.get('assists')
-
+    send_event_data(username, win, kills, deaths, assists)
     url = f'{BASE_URL}/members/action/topup'
     headers = {
         "Authorization": f"Bearer {TOKEN}"
@@ -61,7 +84,7 @@ def topup():
         'topup_ids': str(user_id),
         'topup_value': 0,
         'add_coin': coins,
-        'comment': f'wincoin cs2({kills} kills, {deaths} deaths, {assists} assists)'
+        'comment': f'cs2({kills} kills, {deaths} deaths, {assists} assists)'
     }
 
     response = requests.post(url, headers=headers, json=body)
